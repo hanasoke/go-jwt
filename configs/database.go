179fb597d6308487ba600975058d09344cae4993
hanasbp <hanasoke@gmail.com>
2025-12-13 18:09:07 +0700

    updated 2 pages

--- configs/database.go
+++ configs/database.go
@@ -1,1 +1,20 @@
 package configs
+
+import (
+	"log"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func ConnectDB() {
+	db, err := gorm.Open(mysql.Open("root:@tcp(127.0.0.1:3306)/go_jwt?parseTime=true"), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	DB = db
+	log.Println("Database connected")
+}
