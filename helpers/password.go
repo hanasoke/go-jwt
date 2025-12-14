6a59bc7b4b3eaf5f3bc0ce5101496cd1d7d7ef8f
hanasbp <hanasoke@gmail.com>
2025-12-14 18:37:51 +0700

    updated password.go

--- helpers/password.go
+++ helpers/password.go
@@ -9,3 +9,7 @@ func HashPassword(password string) (string, error) {
 
 	return string(passwordHash), err
 }
+
+func VerifyPassword(hashPassword, password string) error {
+	return bcrypt.CompareHashAndPassword([]byte(hashPassword), []byte(password))
+}
