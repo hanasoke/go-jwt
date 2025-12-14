56abc5f475eac2b01b2342bc6f4e5493e91879ac
hanasbp <hanasoke@gmail.com>
2025-12-14 17:12:04 +0700

    updated auth.go

--- routes/auth.go
+++ routes/auth.go
@@ -1,1 +1,13 @@
 package routes
+
+import (
+	"go-jwt/controllers"
+
+	"github.com/gorilla/mux"
+)
+
+func AuthRoutes(r *mux.Router) {
+	router := r.PathPrefix("/auth").Subrouter()
+
+	router.HandleFunc("/register", controllers.Register).Methods("POST")
+}
